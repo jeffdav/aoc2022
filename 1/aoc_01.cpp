2072dbdf7c6f0a47fba3382de6abd16de649aefb
Jeffrey Davis <jeffdav@fb.com>
2022-12-02 13:49:15 -0800

    Day 1 and Day 2.

---
+++ 1/aoc_01.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+int main (int argc, char *argv[]) { 
+  std::ifstream file("input_01.txt");
+  if (!file.is_open()) {
+    std::cout << "File open failure. :(" << std::endl;
+    exit(1);
+  }
+
+  std::vector<int64_t> elf_calories;
+  std::string line;
+  int64_t sum = 0;
+  while(std::getline(file, line)) {
+    if (std::all_of(line.begin(), line.end(), isspace)) {
+      elf_calories.emplace_back(sum);
+      sum = 0;
+    } else {
+      std::stringstream ss;
+      int64_t number;
+      ss << line;
+      ss >> number;
+      sum += number;
+    }
+  }
+
+  std::cout << "Max value is: " << *std::max_element(elf_calories.begin(), elf_calories.end()) << std::endl;
+
+  std::sort(elf_calories.begin(), elf_calories.end());
+
+  // assert(elf_calories.length >= 3);
+
+  int64_t top_three_sum = 0;
+  std::reverse_iterator rit = elf_calories.rbegin();
+  for (int i = 0; i < 3; i++) {
+    top_three_sum += *rit;
+    rit++;
+  }
+
+  std::cout << "Top three sum is: " << top_three_sum << std::endl;
+
+  file.close();
+}
---
+++ 2/aoc_02.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+typedef enum _ThrowType {
+  ROCK = 1,      // A, X
+  PAPER = 2,     // B, Y
+  SCISSORS = 3,  // C, Z
+} ThrowType;
+
+typedef enum _Result {
+  LOSE = 0,
+  DRAW = 3,
+  WIN = 6,
+} Result;
+
+// Part one: X = Rock, Y = Paper, Z = Scissors
+// std::map<std::string, int64_t> results = {
+//   // Enemy plays Rock:
+//   {"A X", DRAW + ROCK },
+//   {"A Y", WIN + PAPER },
+//   {"A Z", LOSE + SCISSORS },
+
+//   // Enemy plays Paper:
+//   {"B X", LOSE + ROCK },
+//   {"B Y", DRAW + PAPER},
+//   {"B Z", WIN + SCISSORS },
+
+//   // Enemy plays Scissors:
+//   {"C X", WIN + ROCK },
+//   {"C Y", LOSE + PAPER },
+//   {"C Z", DRAW + SCISSORS },
+// };
+
+// Part two: X = Lose, Y = Draw, Z = Win
+std::map<std::string, int64_t> results = {
+  // Enemy plays Rock:
+  {"A X", LOSE + SCISSORS },
+  {"A Y", DRAW + ROCK },
+  {"A Z", WIN + PAPER },
+
+  // Enemy plays Paper:
+  {"B X", LOSE + ROCK },
+  {"B Y", DRAW + PAPER},
+  {"B Z", WIN + SCISSORS },
+
+  // Enemy plays Scissors:
+  {"C X", LOSE + PAPER },
+  {"C Y", DRAW + SCISSORS },
+  {"C Z", WIN + ROCK },
+};
+
+int main (int argc, char *argv[]) { 
+  std::ifstream file("input_02.txt");
+  if (!file.is_open()) {
+    std::cout << "File open failure. :(" << std::endl;
+    exit(1);
+  }
+
+  std::string line;
+  int64_t score = 0;
+  while(std::getline(file, line)) {
+    score += results[line];
+  }
+
+  std::cout << "Total score is: " << score << std::endl;
+}
