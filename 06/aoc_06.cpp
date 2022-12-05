a8208418e0015e6e5a119ab08c858b9c4a5d6bc2
Jeffrey Davis <jeffdav@fb.com>
2022-12-05 12:13:14 -0800

    AoC 06 boilerplate.

---
+++ 06/aoc_06.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void solve_part_one() {
+  std::ifstream file("input_06.txt");
+  if (!file.is_open()) {
+    std::cout << "File open failure. :(" << std::endl;
+    exit(1);
+  }
+
+  std::string line;
+  while(std::getline(file, line)) {
+  }
+
+  std::cout << "1: " << std::endl;
+
+  file.close();
+}
+
+void solve_part_two() {
+  std::ifstream file("input_06.txt");
+  if (!file.is_open()) {
+    std::cout << "File open failure. :(" << std::endl;
+    exit(1);
+  }
+
+  std::string line;
+  while(std::getline(file, line)) {
+  }
+
+  std::cout << "2: " << std::endl;
+
+  file.close();
+}
+
+int main(int argc, char *argv[]) { 
+  solve_part_one();
+  solve_part_two();
+}
