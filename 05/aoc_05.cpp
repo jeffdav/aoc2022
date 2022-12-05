2fc33383e84b3bc607a8721e0eb4aaeba55467ac
Jeffrey Davis <jeffdav@fb.com>
2022-12-04 21:55:36 -0800

    AoC 05.

--- 05/aoc_05.cpp
+++ 05/aoc_05.cpp
@@ -1,7 +1,107 @@
+#include <array>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stack>
+#include <vector>
+
+template <class T>
+std::vector<T> split(const T& str, char split_char)
+{
+    std::vector<T> parts;
+    typename T::size_type start_index = 0;
+    typename T::size_type end_index;
+
+    while ((end_index = str.find_first_of(split_char, start_index)) != T::npos) {
+        parts.push_back(str.substr(start_index, end_index - start_index));
+        start_index = end_index + 1;
+    }
+    parts.push_back(str.substr(start_index, T::npos));
+
+    return parts;
+}
+
+int64_t asInt(const std::string& str) {
+    std::stringstream ss;
+    int64_t number;
+    ss << str;
+    ss >> number;
+    return number;
+}
+
+typedef enum _cmds {
+  MOVE = 1,
+  FROM = 3,
+  TO = 5
+} cmds_t;
+
+std::array<std::stack<char>, 9> stacks;
+
+void init_stacks() {
+  for (int i = 0; i < 9; i++) {
+    while (!stacks[i].empty()) {
+      stacks[i].pop();
+    }
+  }
+
+  stacks[8].push('J');
+  stacks[8].push('S');
+  stacks[8].push('Q');
+  stacks[8].push('C');
+  stacks[8].push('W');
+  stacks[8].push('D');
+  stacks[8].push('M');
+  stacks[7].push('G');
+  stacks[7].push('T');
+  stacks[7].push('R');
+  stacks[7].push('C');
+  stacks[7].push('J');
+  stacks[7].push('Q');
+  stacks[7].push('S');
+  stacks[7].push('N');
+  stacks[6].push('L');
+  stacks[6].push('N');
+  stacks[6].push('Q');
+  stacks[6].push('B');
+  stacks[6].push('J');
+  stacks[6].push('V');
+  stacks[5].push('F');
+  stacks[5].push('W');
+  stacks[5].push('J');
+  stacks[4].push('M');
+  stacks[4].push('P');
+  stacks[4].push('D');
+  stacks[4].push('V');
+  stacks[4].push('F');
+  stacks[3].push('L');
+  stacks[3].push('G');
+  stacks[3].push('W');
+  stacks[3].push('S');
+  stacks[3].push('Z');
+  stacks[3].push('J');
+  stacks[3].push('D');
+  stacks[3].push('N');
+  stacks[2].push('F');
+  stacks[2].push('W');
+  stacks[2].push('R');
+  stacks[2].push('T');
+  stacks[2].push('S');
+  stacks[2].push('Q');
+  stacks[2].push('B');
+  stacks[1].push('W');
+  stacks[1].push('D');
+  stacks[1].push('B');
+  stacks[1].push('G');
+  stacks[0].push('B');
+  stacks[0].push('V');
+  stacks[0].push('S');
+  stacks[0].push('N');
+  stacks[0].push('T');
+  stacks[0].push('C');
+  stacks[0].push('H');
+  stacks[0].push('Q');  
+}
 
 void solve_part_one() {
   std::ifstream file("input_05.txt");
@@ -10,9 +110,31 @@ void solve_part_one() {
     exit(1);
   }
 
+  init_stacks();
+
   std::string line;
   while(std::getline(file, line)) {
+    auto cmds = split(line, ' ');
+    int64_t move = asInt(cmds[MOVE]);
+    int64_t from = asInt(cmds[FROM]);
+    int64_t to = asInt(cmds[TO]);
+
+    // Our stacks array is zero-based.
+    from--;
+    to--;
+
+    for (int i = 0; i < move; i++) {
+      stacks[to].push(stacks[from].top());
+      stacks[from].pop();
+    }
   }
+
+  std::string result;
+  for (int i = 0; i < stacks.size(); i++) {
+    result += stacks[i].top();
+  }
+
+  std::cout << "1: top items are: " << result << std::endl;
 
   file.close();
 }
@@ -24,9 +146,36 @@ void solve_part_two() {
     exit(1);
   }
 
+  init_stacks();
+
+  std::stack<char> crane;
   std::string line;
   while(std::getline(file, line)) {
+    auto cmds = split(line, ' ');
+    int64_t move = asInt(cmds[MOVE]);
+    int64_t from = asInt(cmds[FROM]);
+    int64_t to = asInt(cmds[TO]);    
+
+    // Our stacks array is zero-based.
+    from--;
+    to--;
+
+    for (int i = 0; i < move; i++) {
+      crane.push(stacks[from].top());
+      stacks[from].pop();
+    }
+    for (int i = 0; i < move; i++) {
+      stacks[to].push(crane.top());
+      crane.pop();
+    }    
   }
+
+  std::string result;
+  for (int i = 0; i < stacks.size(); i++) {
+    result += stacks[i].top();
+  }
+
+  std::cout << "2: top items are: " << result << std::endl;
 
   file.close();
 }
