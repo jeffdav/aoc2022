0002e6ff842e6f0cda0968643186cb6f5a607ddf
Jeffrey Davis <jeffdav@fb.com>
2022-12-08 22:59:50 -0800

    Tabs -> spaces.

--- 09/aoc_09.cpp
+++ 09/aoc_09.cpp
@@ -10,8 +10,8 @@
 #include "../common/string_utils.h"
 
 typedef enum _cmd_t {
-	DIR,
-	DIST,
+  DIR,
+  DIST,
 } cmd_t;
 
 typedef std::pair<int64_t, int64_t> end_t;
@@ -19,26 +19,26 @@ typedef std::pair<int64_t, int64_t> end_t;
 std::set<std::pair<int64_t, int64_t>> tail_locations;
 
 void updateTail(const std::string& dir, const end_t& head, end_t& tail) {
-	int64_t dx = head.first - tail.first;
-	int64_t dy = head.second - tail.second;
+  int64_t dx = head.first - tail.first;
+  int64_t dy = head.second - tail.second;
 
-	if (std::abs(dx) <= 1 && std::abs(dy) <= 1) {
-		return;
-	}
+  if (std::abs(dx) <= 1 && std::abs(dy) <= 1) {
+    return;
+  }
 
-	if (dir == "U") {
-		tail.second = head.second - 1;
-		tail.first = head.first;
-	} else if (dir == "D") {
-		tail.second = head.second + 1;
-		tail.first = head.first;		
-	} else if (dir == "L") {
-		tail.first = head.first + 1;
-		tail.second = head.second;
-	} else if (dir == "R") {
-		tail.first = head.first - 1;
-		tail.second = head.second;
-	}
+  if (dir == "U") {
+    tail.second = head.second - 1;
+    tail.first = head.first;
+  } else if (dir == "D") {
+    tail.second = head.second + 1;
+    tail.first = head.first;		
+  } else if (dir == "L") {
+    tail.first = head.first + 1;
+    tail.second = head.second;
+  } else if (dir == "R") {
+    tail.first = head.first - 1;
+    tail.second = head.second;
+  }
 
   tail_locations.insert(tail);
 }
@@ -53,29 +53,29 @@ void solve_part_one() {
   end_t head = {0, 0};
   end_t tail = {0, 0};
 
-	tail_locations.insert(tail);
+  tail_locations.insert(tail);
 
   std::string line;
   while(std::getline(file, line)) {
     auto r = split(line, ' ');
-		auto dir = r[DIR];
-		auto dist = asInt(r[DIST]);
+    auto dir = r[DIR];
+    auto dist = asInt(r[DIST]);
 
-		for (int64_t i = 0; i < dist; i++) {
-			if (dir == "U") {
-				head.second++;
-				updateTail(dir, head, tail);
-			} else if (dir == "D") {
-				head.second--;
-				updateTail(dir, head, tail);
-			} else if (dir == "L") {
-				head.first--;
-				updateTail(dir, head, tail);
-			} else if (dir == "R") {
-				head.first++;
-				updateTail(dir, head, tail);
-			} 
-		}
+    for (int64_t i = 0; i < dist; i++) {
+      if (dir == "U") {
+        head.second++;
+        updateTail(dir, head, tail);
+      } else if (dir == "D") {
+        head.second--;
+        updateTail(dir, head, tail);
+      } else if (dir == "L") {
+        head.first--;
+        updateTail(dir, head, tail);
+      } else if (dir == "R") {
+        head.first++;
+        updateTail(dir, head, tail);
+      } 
+    }
   }
 
   std::cout << "1: " << tail_locations.size() << std::endl;
@@ -84,59 +84,59 @@ void solve_part_one() {
 }
 
 void updateSegment(std::string dir, const end_t& head, end_t& tail) {
-	int64_t dx = head.first - tail.first;
-	int64_t dy = head.second - tail.second;
+  int64_t dx = head.first - tail.first;
+  int64_t dy = head.second - tail.second;
 
-	if (std::abs(dx) <= 1 && std::abs(dy) <= 1) {
-		return;
-	}
+  if (std::abs(dx) <= 1 && std::abs(dy) <= 1) {
+    return;
+  }
 
-	if (dx == 2 && dy == 2) {
-		tail.first++;
-		tail.second++;
-	} else if (dx == 2 && dy == -2) {
-		tail.first++;
-		tail.second--;
-	} else if (dx == -2 && dy == 2) {
-		tail.first--;
-		tail.second++;
-	} else if (dx == -2 && dy == -2) {
-		tail.first--;
-		tail.second--;
-	} else {
-		if (dx == 2) {
-			dir = "R";
-		} else if (dx == -2) {
-			dir = "L";
-		} else if (dy == 2) {
-			dir =  "U";
-		} else if (dy == -2) {
-			dir = "D";
-		}
+  if (dx == 2 && dy == 2) {
+    tail.first++;
+    tail.second++;
+  } else if (dx == 2 && dy == -2) {
+    tail.first++;
+    tail.second--;
+  } else if (dx == -2 && dy == 2) {
+    tail.first--;
+    tail.second++;
+  } else if (dx == -2 && dy == -2) {
+    tail.first--;
+    tail.second--;
+  } else {
+    if (dx == 2) {
+      dir = "R";
+    } else if (dx == -2) {
+      dir = "L";
+    } else if (dy == 2) {
+      dir =  "U";
+    } else if (dy == -2) {
+      dir = "D";
+    }
 
-		if (dir == "U") {
-			tail.second = head.second - 1;
-			tail.first = head.first;
-		} else if (dir == "D") {
-			tail.second = head.second + 1;
-			tail.first = head.first;
-		} else if (dir == "L") {
-			tail.first = head.first + 1;
-			tail.second = head.second;
-		} else if (dir == "R") {
-			tail.first = head.first - 1;
-			tail.second = head.second;
-		}
-	}
+    if (dir == "U") {
+      tail.second = head.second - 1;
+      tail.first = head.first;
+    } else if (dir == "D") {
+      tail.second = head.second + 1;
+      tail.first = head.first;
+    } else if (dir == "L") {
+      tail.first = head.first + 1;
+      tail.second = head.second;
+    } else if (dir == "R") {
+      tail.first = head.first - 1;
+      tail.second = head.second;
+    }
+  }
 }
 
 void updateRope(const std::string& dir, std::array<end_t, 10>& r) {
-	std::string d{dir};
-	for (int i = 0; i < r.size() - 1; i++) {
-		updateSegment(d, r[i], r[i + 1]);
-	}
+  std::string d{dir};
+  for (int i = 0; i < r.size() - 1; i++) {
+    updateSegment(d, r[i], r[i + 1]);
+  }
 
-	tail_locations.insert(r.back());
+  tail_locations.insert(r.back());
 }
 
 void solve_part_two() {
@@ -146,36 +146,36 @@ void solve_part_two() {
     exit(1);
   }
 
-	tail_locations.clear();
+  tail_locations.clear();
 
-	std::array<end_t, 10> rope;
+  std::array<end_t, 10> rope;
 
-	for (int i = 0; i < rope.size(); i++) {
-		rope[i].first = 0;
-		rope[i].second = 0;
-	}
+  for (int i = 0; i < rope.size(); i++) {
+    rope[i].first = 0;
+    rope[i].second = 0;
+  }
 
   std::string line;
   while(std::getline(file, line)) {
     auto r = split(line, ' ');
-		auto dir = r[DIR];
-		auto dist = asInt(r[DIST]);
+    auto dir = r[DIR];
+    auto dist = asInt(r[DIST]);
 
-		for (int64_t i = 0; i < dist; i++) {
-			if (dir == "U") {
-				rope.front().second++;
-				updateRope(dir, rope);
-			} else if (dir == "D") {
-				rope.front().second--;
-				updateRope(dir, rope);
-			} else if (dir == "L") {
-				rope.front().first--;
-				updateRope(dir, rope);
-			} else if (dir == "R") {
-				rope.front().first++;
-				updateRope(dir, rope);
-			} 
-		}
+    for (int64_t i = 0; i < dist; i++) {
+      if (dir == "U") {
+        rope.front().second++;
+        updateRope(dir, rope);
+      } else if (dir == "D") {
+        rope.front().second--;
+        updateRope(dir, rope);
+      } else if (dir == "L") {
+        rope.front().first--;
+        updateRope(dir, rope);
+      } else if (dir == "R") {
+        rope.front().first++;
+        updateRope(dir, rope);
+      } 
+    }
   }
 
   std::cout << "2: " << tail_locations.size() << std::endl;
