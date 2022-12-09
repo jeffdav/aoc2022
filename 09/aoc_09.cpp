f87a468b206665e4f013a567c08e5dc0b4f17608
Jeffrey Davis <jeffdav@fb.com>
2022-12-08 22:58:12 -0800

    AoC 09.

--- 09/aoc_09.cpp
+++ 09/aoc_09.cpp
@@ -1,11 +1,47 @@
+#include <array>
 #include <algorithm>
 #include <iostream>
 #include <fstream>
+#include <set>
 #include <string>
 #include <sstream>
 #include <vector>
 
 #include "../common/string_utils.h"
+
+typedef enum _cmd_t {
+	DIR,
+	DIST,
+} cmd_t;
+
+typedef std::pair<int64_t, int64_t> end_t;
+
+std::set<std::pair<int64_t, int64_t>> tail_locations;
+
+void updateTail(const std::string& dir, const end_t& head, end_t& tail) {
+	int64_t dx = head.first - tail.first;
+	int64_t dy = head.second - tail.second;
+
+	if (std::abs(dx) <= 1 && std::abs(dy) <= 1) {
+		return;
+	}
+
+	if (dir == "U") {
+		tail.second = head.second - 1;
+		tail.first = head.first;
+	} else if (dir == "D") {
+		tail.second = head.second + 1;
+		tail.first = head.first;		
+	} else if (dir == "L") {
+		tail.first = head.first + 1;
+		tail.second = head.second;
+	} else if (dir == "R") {
+		tail.first = head.first - 1;
+		tail.second = head.second;
+	}
+
+  tail_locations.insert(tail);
+}
 
 void solve_part_one() {
   std::ifstream file("input_09.txt");
@@ -14,13 +50,93 @@ void solve_part_one() {
     exit(1);
   }
 
+  end_t head = {0, 0};
+  end_t tail = {0, 0};
+
+	tail_locations.insert(tail);
+
   std::string line;
   while(std::getline(file, line)) {
+    auto r = split(line, ' ');
+		auto dir = r[DIR];
+		auto dist = asInt(r[DIST]);
+
+		for (int64_t i = 0; i < dist; i++) {
+			if (dir == "U") {
+				head.second++;
+				updateTail(dir, head, tail);
+			} else if (dir == "D") {
+				head.second--;
+				updateTail(dir, head, tail);
+			} else if (dir == "L") {
+				head.first--;
+				updateTail(dir, head, tail);
+			} else if (dir == "R") {
+				head.first++;
+				updateTail(dir, head, tail);
+			} 
+		}
   }
 
-  std::cout << "1: " << std::endl;
+  std::cout << "1: " << tail_locations.size() << std::endl;
 
   file.close();
+}
+
+void updateSegment(std::string dir, const end_t& head, end_t& tail) {
+	int64_t dx = head.first - tail.first;
+	int64_t dy = head.second - tail.second;
+
+	if (std::abs(dx) <= 1 && std::abs(dy) <= 1) {
+		return;
+	}
+
+	if (dx == 2 && dy == 2) {
+		tail.first++;
+		tail.second++;
+	} else if (dx == 2 && dy == -2) {
+		tail.first++;
+		tail.second--;
+	} else if (dx == -2 && dy == 2) {
+		tail.first--;
+		tail.second++;
+	} else if (dx == -2 && dy == -2) {
+		tail.first--;
+		tail.second--;
+	} else {
+		if (dx == 2) {
+			dir = "R";
+		} else if (dx == -2) {
+			dir = "L";
+		} else if (dy == 2) {
+			dir =  "U";
+		} else if (dy == -2) {
+			dir = "D";
+		}
+
+		if (dir == "U") {
+			tail.second = head.second - 1;
+			tail.first = head.first;
+		} else if (dir == "D") {
+			tail.second = head.second + 1;
+			tail.first = head.first;
+		} else if (dir == "L") {
+			tail.first = head.first + 1;
+			tail.second = head.second;
+		} else if (dir == "R") {
+			tail.first = head.first - 1;
+			tail.second = head.second;
+		}
+	}
+}
+
+void updateRope(const std::string& dir, std::array<end_t, 10>& r) {
+	std::string d{dir};
+	for (int i = 0; i < r.size() - 1; i++) {
+		updateSegment(d, r[i], r[i + 1]);
+	}
+
+	tail_locations.insert(r.back());
 }
 
 void solve_part_two() {
@@ -30,11 +146,39 @@ void solve_part_two() {
     exit(1);
   }
 
+	tail_locations.clear();
+
+	std::array<end_t, 10> rope;
+
+	for (int i = 0; i < rope.size(); i++) {
+		rope[i].first = 0;
+		rope[i].second = 0;
+	}
+
   std::string line;
   while(std::getline(file, line)) {
+    auto r = split(line, ' ');
+		auto dir = r[DIR];
+		auto dist = asInt(r[DIST]);
+
+		for (int64_t i = 0; i < dist; i++) {
+			if (dir == "U") {
+				rope.front().second++;
+				updateRope(dir, rope);
+			} else if (dir == "D") {
+				rope.front().second--;
+				updateRope(dir, rope);
+			} else if (dir == "L") {
+				rope.front().first--;
+				updateRope(dir, rope);
+			} else if (dir == "R") {
+				rope.front().first++;
+				updateRope(dir, rope);
+			} 
+		}
   }
 
-  std::cout << "2: " << std::endl;
+  std::cout << "2: " << tail_locations.size() << std::endl;
 
   file.close();
 }
